57a1272c720478adab1f20656a60017d65d148eb
jxareas <areasjx@gmail.com>
2022-03-13 02:46:39 -0600

    Complete Generic Layers

---
+++ src/main/java/com/jonareas/movies/controllers/BaseController.java
@@ -0,0 +1,22 @@
+package com.jonareas.movies.controllers;
+
+import com.jonareas.movies.entities.BaseEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.Serializable;
+
+public interface BaseController<T extends BaseEntity, ID extends Serializable> {
+
+    public ResponseEntity<?> getAll();
+
+    public ResponseEntity<?> save(@RequestBody T entity);
+
+    public ResponseEntity<?> update(@PathVariable ID id, @RequestBody T entity);
+
+    public ResponseEntity<?> delete(@PathVariable ID id);
+
+
+
+}
---
+++ src/main/java/com/jonareas/movies/entities/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.jonareas.movies.entities;
+
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public interface BaseEntity extends Serializable {
+
+
+
+
+
+}
